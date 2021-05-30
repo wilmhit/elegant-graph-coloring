4b89e5766057142c0fa969cc3274fb6b07ba415e
Jcob Chodubski <jchodubski@pm.me>
2021-05-30 14:02:38 +0200

    Basic graph drawing

--- graph-coloring/results_processing.py
+++ graph-coloring/results_processing.py
@@ -1,4 +1,16 @@
-from custom_types import Idk
+from custom_types import Graph as AdjMatrix
+import networkx
+from matplotlib import pyplot
 
-def output_results(results: Idk) -> None:
-    print(results)
+def get_node_array(graph: AdjMatrix):
+    return [x for x in range(len(graph))]
+
+def get_edge_array(graph: AdjMatrix):
+    return [(1,2)] # TODO
+
+def output_results(results: AdjMatrix) -> None:
+    graph = networkx.Graph()
+    graph.add_nodes_from(get_node_array(results))
+    graph.add_edges_from(get_edge_array(results))
+    networkx.draw(graph)
+    pyplot.show()
