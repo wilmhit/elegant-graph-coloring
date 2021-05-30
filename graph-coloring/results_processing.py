33941c86f7739a8e202d80ae08fa69d8b0a6e0f8
Jcob Chodubski <jchodubski@pm.me>
2021-05-30 14:33:41 +0200

    Added edges

--- graph-coloring/results_processing.py
+++ graph-coloring/results_processing.py
@@ -6,11 +6,18 @@ def get_node_array(graph: AdjMatrix):
     return [x for x in range(len(graph))]
 
 def get_edge_array(graph: AdjMatrix):
-    return [(1,2)] # TODO
+    edges = []
+    for row_num, row in enumerate(graph):
+        for column_num, element in enumerate(row):
+            if column_num < row_num:
+                continue
+            if element:
+                edges.append((row_num, column_num, {"weight": element}))
+    return edges
 
 def output_results(results: AdjMatrix) -> None:
     graph = networkx.Graph()
     graph.add_nodes_from(get_node_array(results))
     graph.add_edges_from(get_edge_array(results))
-    networkx.draw(graph)
+    networkx.draw_spring(graph)
     pyplot.show()
