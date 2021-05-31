03f97618a0e637ed395a9970454ae62cfc161f9a
Jcob Chodubski <jchodubski@pm.me>
2021-05-31 15:16:33 +0200

    Fixed labels

--- graph_coloring/results_processing.py
+++ graph_coloring/results_processing.py
@@ -1,27 +1,43 @@
-from custom_types import AdjMatrix
+from .custom_types import AdjMatrix
 import networkx
 from matplotlib import pyplot
-from typing import List, Any, Tuple
+from typing import List, Any, Tuple, Dict
 
+Edge = Tuple[int, int]
 
 def get_node_array(graph: AdjMatrix) -> List[int]:
     return [x for x in range(len(graph))]
 
 
-def get_edge_array(graph: AdjMatrix) -> List[Tuple[int, int, Any]]:
-    edges = []
+def get_edges_and_labels(graph: AdjMatrix) -> Tuple[List[Edge], Dict[Edge, int]]:
+    edges: List[Edge] = []
+    labels: Dict[Edge, int] = {}
     for row_num, row in enumerate(graph):
-        for column_num, element in enumerate(row):
+        for column_num, element  in enumerate(row):
             if column_num < row_num:
                 continue
             if element:
-                edges.append((row_num, column_num, {"weight": element}))
-    return edges
+                edge = (row_num, column_num)
+                edges.append(edge)
+                labels[edge] = element
+
+    return edges, labels
 
 
 def output_results(results: AdjMatrix) -> None:
+    edges, labels = get_edges_and_labels(results)
+    nodes = get_node_array(results)
+
     graph = networkx.Graph()
-    graph.add_nodes_from(get_node_array(results))
-    graph.add_edges_from(get_edge_array(results))
-    networkx.draw_spring(graph)
+    graph.add_nodes_from(nodes)
+    graph.add_edges_from(edges)
+    layout = networkx.spring_layout(graph)
+
+    networkx.draw_networkx_nodes(graph, layout)
+    networkx.draw_networkx_edges(graph, layout)
+    networkx.draw_networkx_labels(graph, layout)
+    networkx.draw_networkx_edge_labels(graph,
+                                       layout,
+                                       edge_labels=labels)
+    pyplot.tight_layout() # Removes huge margins
     pyplot.show()
