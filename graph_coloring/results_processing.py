a3bb9ce081f5c388296dead3cbc152c1db9d3b9f
Jcob Chodubski <jchodubski@pm.me>
2021-05-31 21:35:07 +0200

    Add graphical graph output (#6)
    * Basic graph drawing
    
    * Added edges
    
    * Format and fix types
    
    * Fixed labels
    
    * Format
    
    * Remove margins

--- graph_coloring/results_processing.py
+++ graph_coloring/results_processing.py
@@ -1,5 +1,42 @@
 from .custom_types import AdjMatrix
+import networkx
+from matplotlib import pyplot
+from typing import List, Any, Tuple, Dict
+
+Edge = Tuple[int, int]
+
+
+def get_node_array(graph: AdjMatrix) -> List[int]:
+    return [x for x in range(len(graph))]
+
+
+def get_edges_and_labels(graph: AdjMatrix) -> Tuple[List[Edge], Dict[Edge, int]]:
+    edges: List[Edge] = []
+    labels: Dict[Edge, int] = {}
+    for row_num, row in enumerate(graph):
+        for column_num, element in enumerate(row):
+            if column_num < row_num:
+                continue
+            if element:
+                edge = (row_num, column_num)
+                edges.append(edge)
+                labels[edge] = element
+
+    return edges, labels
 
 
 def output_results(results: AdjMatrix) -> None:
-    print(results)
+    edges, labels = get_edges_and_labels(results)
+    nodes = get_node_array(results)
+
+    graph = networkx.Graph()
+    graph.add_nodes_from(nodes)
+    graph.add_edges_from(edges)
+    layout = networkx.spring_layout(graph)
+
+    networkx.draw_networkx_nodes(graph, layout)
+    networkx.draw_networkx_edges(graph, layout)
+    networkx.draw_networkx_labels(graph, layout)
+    networkx.draw_networkx_edge_labels(graph, layout, edge_labels=labels)
+    pyplot.tight_layout(pad=0)
+    pyplot.show()
