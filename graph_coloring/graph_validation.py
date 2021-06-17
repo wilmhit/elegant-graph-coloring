0a48b89e821abbf3004f79f2eea020be60fc85cc
Jcob Chodubski <jchodubski@pm.me>
2021-06-17 16:21:21 +0200

    Add additional validation (#10)
    * Added additional validation
    
    * Add message
    
    * Fix typecheck
    
    * Change message

--- graph_coloring/graph_validation.py
+++ graph_coloring/graph_validation.py
@@ -9,6 +9,8 @@ def get_error_if_invalid(graph: AdjMatrix) -> Optional[str]:
         return "The provided graph is not square"
     if not has_zeros_on_diagonal(graph):
         return "The provided graph should only have zeros on its diagonal"
+    if not edges_greater_equal_vertices(graph):
+        return "The provided graph must have more edges than vertices"
     return None
 
 
@@ -19,3 +21,13 @@ def is_square(graph: AdjMatrix) -> bool:
 
 def has_zeros_on_diagonal(graph: AdjMatrix) -> bool:
     return all(graph[x][x] == 0 for x in range(len(graph)))
+
+
+def edges_greater_equal_vertices(graph: AdjMatrix) -> bool:
+    vertices = len(graph)
+    edges = 0
+    for row_num in range(len(graph)):
+        for col_num in range(row_num, len(graph)):
+            if graph[row_num][col_num] > 0:
+                edges += 1
+    return edges >= vertices
---
+++ tests/test_graph_validation.py
@@ -0,0 +1,13 @@
+import pytest
+from graph_coloring.custom_types import AdjMatrix
+from graph_coloring.graph_validation import edges_greater_equal_vertices
+
+
+@pytest.mark.parametrize("graph", [[[0, 1, 1], [1, 0, 1], [1, 1, 0]]])
+def test_edge_counting_passing(graph: AdjMatrix):
+    assert edges_greater_equal_vertices(graph)
+
+
+@pytest.mark.parametrize("graph", [[[0, 1, 0], [1, 0, 1], [0, 1, 0]]])
+def test_edge_counting_failing(graph: AdjMatrix):
+    assert not edges_greater_equal_vertices(graph)
